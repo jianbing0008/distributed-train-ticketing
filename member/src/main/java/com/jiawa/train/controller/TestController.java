80967ec4747999cc3b04374379cd40b1b0e9be7e
JianBing <jianbing0828@163.com>
2024-11-16 15:09:56 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.jiawa.train;
+package com.jiawa.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello world!!!";
+        return "hello world!";
     }
 }
