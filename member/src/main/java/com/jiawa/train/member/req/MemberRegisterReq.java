bc23ea04eacba4b32724126d9a47d65babf88258
JianBing <jianbing0828@163.com>
2024-11-17 16:25:34 +0800

    4.19 集成校验框架validation

--- member/src/main/java/com/jiawa/train/member/req/MemberRegisterReq.java
+++ member/src/main/java/com/jiawa/train/member/req/MemberRegisterReq.java
@@ -1,8 +1,16 @@
 package com.jiawa.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@ToString
 public class MemberRegisterReq {
+    @NotBlank(message = "【手机号】不能为空")
     private String mobile;
 }
