0c288091076f8a1ff2ce69d8c46ee587741d1618
JianBing <jianbing0828@163.com>
2024-11-17 14:01:30 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.controller;
 import com.jiawa.train.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,12 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
     @GetMapping("/count")
-    public int count() {
+    public Integer count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
