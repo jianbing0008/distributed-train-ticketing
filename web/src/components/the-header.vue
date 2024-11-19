a530c70cc0b7fc3311eacd54fc8aa2ce5469bf49
JianBing <jianbing0828@163.com>
2024-11-19 17:19:30 +0800

    6.5 使用vuex(store)来保存登录信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,12 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+    <div style="float: right; color: white;">
+      欢迎您访问煎饼制作的铁路12306：{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login">
+        退出登录
+      </router-link>
+    </div>
     <a-menu
         v-model:selectedKeys="selectedKeys1"
         theme="dark"
@@ -16,17 +22,19 @@
 
 <script>
 import {defineComponent, ref,} from 'vue';
+import store from "@/store";
 
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
+    let member = store.state.member;
 
 
 
     return {
       selectedKeys1: ref(['2']),
-
+      member
     };
   },
 });
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -49,6 +49,7 @@ import { defineComponent, reactive } from 'vue';
 import axios from "axios";
 import {notification} from "ant-design-vue";
 import { useRouter } from 'vue-router'
+import store from "@/store";
 
 export default defineComponent({
   name: "login-view",
@@ -98,6 +99,7 @@ export default defineComponent({
           })
           console.log("登录成功： ",data.content);
           router.push('/'); // 登录成功，跳转到控制台主页
+          store.commit('setMember',data.content)
         } else {
           notification.error({
             description: data.message
