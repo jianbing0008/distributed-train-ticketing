70f9fe0a546b95171b4ea6a6c6bb67b2723524a3
JianBing <jianbing0828@163.com>
2024-11-19 19:12:04 +0800

    6.6 vuex(store)配合h5的session解决浏览器刷新问题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,14 +1,18 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "MEMBER"
+
 export default createStore({
   state: {
-    member:{}
+    // {}避免空指针异常
+    member: window.SessionStorage.get(MEMBER) || {}
   },
   getters: {
   },
   mutations: {
     setMember(state,_member){
       state.member = _member
+      window.SessionStorage.set(MEMBER,_member)
     }
   },
   actions: {
