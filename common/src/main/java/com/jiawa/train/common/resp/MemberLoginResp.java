eec84b1003f5943a7e1c246897edaad4485c5402
JianBing <jianbing0828@163.com>
2024-11-21 21:32:17 +0800

    7.5 使用本地线程存储基本用户信息 注：减少获取每一次获取用户基本信息的接口编写

---
+++ common/src/main/java/com/jiawa/train/common/resp/MemberLoginResp.java
@@ -0,0 +1,20 @@
+package com.jiawa.train.common.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginResp {
+    private Long id;
+
+    private String mobile;
+
+    private String token;
+
+
+}
