3eb018aa2f61c3e88ec0793cf64f153f4409384d
JianBing <jianbing0828@163.com>
2024-11-23 17:08:43 +0800

    7.6 通过监听实现菜单同步

--- web/src/components/the-sider.vue
+++ web/src/components/the-sider.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout-sider width="200" style="background: #fff">
     <a-menu
-        v-model:selectedKeys="selectedKeys2"
+        v-model:selectedKeys="selectedKeys"
         v-model:openKeys="openKeys"
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
@@ -21,17 +21,24 @@
 </template>
 
 <script>
-import {defineComponent, ref,} from 'vue';
+import {defineComponent, ref, watch} from 'vue';
+import router from "@/router";
 
 
 export default defineComponent({
   name: "the-sider-view",
   setup() {
+    const selectedKeys = ref([]);
 
+    watch(() => router.currentRoute.value.path, (newVal) => {
+      console.log("watch", newVal);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newVal);
+    },{immediate:true});
 
 
     return {
-      selectedKeys2: ref(['1']),
+      selectedKeys,
       openKeys: ref(['sub1']),
     };
   },
