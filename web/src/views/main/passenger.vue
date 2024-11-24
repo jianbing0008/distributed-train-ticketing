a3cc9cad2ccc0892510f23d335925c59f0e8dad6
JianBing <jianbing0828@163.com>
2024-11-24 15:27:17 +0800

    7.16 删除乘车用户前后端实现

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -18,6 +18,12 @@
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
         <a-space>
+          <a-popconfirm
+              title="删除后不可恢复，确认删除？"
+              @confirm="onDelete(record)"
+              ok-text="确认" cancel-text="取消">
+            <a style="color: red">删除</a>
+          </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
         </a-space>
       </template>
@@ -126,6 +132,22 @@ export default defineComponent({
       visible.value = true;
     };
 
+    // 删除乘客信息的方法
+    const onDelete = (record) => {
+      axios.delete("/member/passenger/delete/" + record.id).then(response => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "删除成功"});
+          handlerQuery({ // 删除成功后刷新数据
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        } else {
+          notification.error({description: data.message})
+        }
+      })
+    };
+
     // 保存乘客信息的方法
     const handleOk = () => {
       axios.post("/member/passenger/save", passenger.value
@@ -194,6 +216,7 @@ export default defineComponent({
       visible,
       onAdd,
       onEdit,
+      onDelete,
       handleOk,
       passenger,
       passengers,
