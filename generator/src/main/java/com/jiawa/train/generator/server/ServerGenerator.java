c7d3e8d4c18806b29c9d5ab35d24284a57b4316a
JianBing <jianbing0828@163.com>
2024-11-28 14:42:27 +0800

    8.4 集成dom4j,读取pom.xml，得到持久层generator.xml

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,23 +1,34 @@
 package com.jiawa.train.generator.server;
 
 
-import com.jiawa.train.generator.util.FreemarkerUtil;
-import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerGenerator {
     static String toPath = "generator\\src\\main\\java\\com\\jiawa\\train\\generator\\test\\";
+    static String pomPath = "generator\\pom.xml";
     static {
         new File(toPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("domain","Test");
-        FreemarkerUtil.generator(toPath + "Test.java",param);
+    public static void main(String[] args) throws Exception{
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain","Test");
+//        FreemarkerUtil.generator(toPath + "Test.java",param);
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        //读取定义好的pom文件
+        Document document = saxReader.read(pomPath);
+        //使用XPath快速定位节点和属性  //：从根目录下寻找，pom是xml命名空间  configuration:在命名空间下寻找configuration的节点
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
     }
 }
