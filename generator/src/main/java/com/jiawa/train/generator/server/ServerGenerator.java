dcf4117bb2bbc5f9408bab5722f7b7fec30d22e7
JianBing <jianbing0828@163.com>
2024-11-28 16:09:54 +0800

    8.5 使用PassengerController制作模板,生成controller成动

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -2,20 +2,22 @@ package com.jiawa.train.generator.server;
 
 
 import com.jiawa.train.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String servicePath = "[module]/src/main/java/com/jiawa/train/[module]/service/";
+    static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/";
     static String pomPath = "generator\\pom.xml";
     static {
-        new File(servicePath).mkdirs();
+        new File(serverPath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception{
@@ -26,8 +28,8 @@ public class ServerGenerator {
                                      .replace(".xml","");
 
         System.out.println("module: " + module);
-        servicePath = servicePath.replace("[module]",module);
-        System.out.println("servicePath: " + servicePath);
+        serverPath = serverPath.replace("[module]",module);
+        System.out.println("servicePath: " + serverPath);
 
         // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
@@ -56,8 +58,25 @@ public class ServerGenerator {
         param.put("do_main", do_main);
         System.out.println("组装参数：" + param);
 
-        FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(servicePath + Domain + "Service.java",param);
+        gen(Domain, param,"service");
+        gen(Domain, param,"controller");
+    }
+
+    /**
+     * 生成,想生成啥就改动里面的target就行
+     * @param Domain
+     * @param param
+     * @throws IOException
+     * @throws TemplateException
+     */
+    private static void gen(String Domain, Map<String, Object> param,String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String toPath = serverPath + target + "/";
+        new File(toPath).mkdirs();
+        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
+        String fileName = toPath + Domain + Target + ".java";
+        System.out.println("开始生成: " + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
