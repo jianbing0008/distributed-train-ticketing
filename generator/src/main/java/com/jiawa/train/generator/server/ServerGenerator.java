94a2205b6b114780cd315ce63260fc22ddfb616a
JianBing <jianbing0828@163.com>
2024-11-28 17:22:34 +0800

    8.7 DbUtile读取表字段信息

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,6 +1,8 @@
 package com.jiawa.train.generator.server;
 
 
+import com.jiawa.train.generator.util.DbUtil;
+import com.jiawa.train.generator.util.Field;
 import com.jiawa.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -11,6 +13,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -34,11 +37,23 @@ public class ServerGenerator {
         // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
-        System.out.println(table);
+        System.out.println("table: "+ table);
         //读取table节点后拿到两个属性 tableName与domainOjectName
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        //从配置文件获取连接数据
+        Node jdbcConnection = document.selectSingleNode("//jdbcConnection");
+        System.out.println(jdbcConnection);
+        Node driverClass = jdbcConnection.selectSingleNode("@driverClass");
+        Node connectionURL = jdbcConnection.selectSingleNode("@connectionURL");
+        Node userId = jdbcConnection.selectSingleNode("@userId");
+        Node password = jdbcConnection.selectSingleNode("@password");
+        System.out.println("connectionURL = " + connectionURL);
+        System.out.println("userId = " + userId);
+        System.out.println("password = " + password);
+        DbUtil.ConnectionInfo(connectionURL.getText(), userId.getText(), password.getText());
 
         // 示例：表名 jian_test
         // Domain = JianTest
@@ -47,6 +62,11 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         // do_main = jian-test  controller层url用到
         String do_main = tableName.getText().replaceAll("_", "-");
+
+        //表中文名  获得表注释
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+        System.out.println(tableName.getName()+"----"+tableName.getText());
 
 
         FreemarkerUtil.initConfig("service.ftl");
@@ -58,8 +78,8 @@ public class ServerGenerator {
         param.put("do_main", do_main);
         System.out.println("组装参数：" + param);
 
-        gen(Domain, param,"service");
-        gen(Domain, param,"controller");
+        //gen(Domain, param,"service");
+        //gen(Domain, param,"controller");
     }
 
     /**
