5e94234bda8642c8ef54845d441b31d5773118b2
JianBing <jianbing0828@163.com>
2024-11-28 14:53:38 +0800

    8.4 集成dom4j,读取pom.xml，得到持久层xml，得到表名和实体名

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -2,6 +2,7 @@ package com.jiawa.train.generator.server;
 
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -21,14 +22,28 @@ public class ServerGenerator {
 //        HashMap<String, Object> param = new HashMap<>();
 //        param.put("domain","Test");
 //        FreemarkerUtil.generator(toPath + "Test.java",param);
+        String generatorPath = getGeneratorPath();
+
+        // 读取table节点
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        //读取table节点后拿到两个属性 tableName与domainOjectName
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
         //读取定义好的pom文件
         Document document = saxReader.read(pomPath);
-        //使用XPath快速定位节点和属性  //：从根目录下寻找，pom是xml命名空间  configuration:在命名空间下寻找configuration的节点
+        //使用XPath快速定位节点和属性  //：从根目录下寻找，pom是xml命名空间  configurationFile:在命名空间下寻找configurationFile的节点
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
+        return node.getText();
     }
 }
