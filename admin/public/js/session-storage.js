724ac0fcc145c133277c1805cfddcc3fec03a10f
JianBing <jianbing0828@163.com>
2024-12-02 16:01:02 +0800

    9.3 创建admin模块，所有的代码都是从web模块复制过来的

---
+++ admin/public/js/session-storage.js
@@ -0,0 +1,41 @@
+// 定义会话存储的键名常量
+SESSION_ORDER = "SESSION_ORDER";
+SESSION_TICKET_PARAMS = "SESSION_TICKET_PARAMS";
+
+/**
+ * 提供对会话存储（sessionStorage）的封装，简化存储操作并增强代码可维护性
+ */
+SessionStorage = {
+    /**
+     * 根据键名获取存储的值
+     * @param {string} key - 要获取值的键名
+     * @returns {any} - 返回解析后的值，如果键不存在或值为undefined，则返回null
+     */
+    get: function (key) {
+        var v = sessionStorage.getItem(key);
+        if (v && typeof(v) !== "undefined" && v !== "undefined") {
+            return JSON.parse(v);
+        }
+    },
+    /**
+     * 将数据存储到会话存储中
+     * @param {string} key - 要存储数据的键名
+     * @param {any} data - 要存储的数据，可以是任何可以被JSON序列化的值
+     */
+    set: function (key, data) {
+        sessionStorage.setItem(key, JSON.stringify(data));
+    },
+    /**
+     * 从会话存储中移除指定键名的数据
+     * @param {string} key - 要移除数据的键名
+     */
+    remove: function (key) {
+        sessionStorage.removeItem(key);
+    },
+    /**
+     * 清空会话存储中的所有数据
+     */
+    clearAll: function () {
+        sessionStorage.clear();
+    }
+};
---
+++ admin/src/components/the-sider.vue
@@ -0,0 +1,51 @@
+<template>
+  <a-layout-sider width="200" style="background: #fff">
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        v-model:openKeys="openKeys"
+        mode="inline"
+        :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import router from "@/router";
+
+
+export default defineComponent({
+  name: "the-sider-view",
+  setup() {
+    const selectedKeys = ref([]);
+
+    watch(() => router.currentRoute.value.path, (newVal) => {
+      console.log("watch", newVal);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newVal);
+    },{immediate:true});
+
+
+    return {
+      selectedKeys,
+      openKeys: ref(['sub1']),
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
