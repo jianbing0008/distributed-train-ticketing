e8f6e705a267f45a55c8a66cbc894b05a4643a51
JianBing <jianbing0828@163.com>
2024-12-05 17:30:37 +0800

    10.7 增加quartz相关的12张表，增加接口来操作quartz任务调度

---
+++ batch/src/main/java/com/jiawa/train/batch/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.jiawa.train.batch.config;
+
+import com.jiawa.train.common.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+
+    }
+}
