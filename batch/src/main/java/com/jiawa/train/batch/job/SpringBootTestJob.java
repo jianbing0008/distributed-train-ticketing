91e515d3e5ce80d4561e292db18f50e36d688eca
JianBing <jianbing0828@163.com>
2024-12-05 15:08:36 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
@@ -1,19 +1,19 @@
-package com.jiawa.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/**
- * 适合单体应用，不适合集群
- * 没法实时更改定时任务状态和策略
- */
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-    @Scheduled(cron = "0/5 * * * * ?") //corn表达式：每5秒执行一次
-    public void test() {
-        //可以加分布式锁，解决集群问题
-        //System.out.println("SpringBootTestJob test!!! ");
-    }
-}
+//package com.jiawa.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 适合单体应用，不适合集群
+// * 没法实时更改定时任务状态和策略
+// */
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//    @Scheduled(cron = "0/5 * * * * ?") //corn表达式：每5秒执行一次
+//    public void test() {
+//        //可以加分布式锁，解决集群问题
+//        //System.out.println("SpringBootTestJob test!!! ");
+//    }
+//}
--- batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -1,14 +1,23 @@
 package com.jiawa.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution // 不允许并发执行
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        //System.out.println("TestJob test!!! ");
+        System.out.println("TestJob test 开始 ");
+        //暂停3秒
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TestJob test 结束 ");
 
     }
 }
