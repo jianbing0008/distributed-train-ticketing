f802ec6484deeee9d74518487f9a0f27764c4703
JianBing <jianbing0828@163.com>
2024-12-14 15:02:29 +0800

    12.14 扣减余票数量，并判断余票是否足够

--- business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
@@ -2,21 +2,26 @@ package com.jiawa.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.jiawa.train.business.domain.DailyTrainTicket;
-import com.jiawa.train.business.enums.ConfirmOrderStatusEnum;
-import com.jiawa.train.business.req.ConfirmOrderDoReq;
-import com.jiawa.train.common.context.LoginMemberContext;
-import com.jiawa.train.common.resp.PageResp;
-import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.business.domain.ConfirmOrder;
 import com.jiawa.train.business.domain.ConfirmOrderExample;
+import com.jiawa.train.business.domain.DailyTrainTicket;
+import com.jiawa.train.business.enums.ConfirmOrderStatusEnum;
+import com.jiawa.train.business.enums.SeatTypeEnum;
 import com.jiawa.train.business.mapper.ConfirmOrderMapper;
+import com.jiawa.train.business.req.ConfirmOrderDoReq;
 import com.jiawa.train.business.req.ConfirmOrderQueryReq;
+import com.jiawa.train.business.req.ConfirmOrderTicketReq;
 import com.jiawa.train.business.resp.ConfirmOrderQueryResp;
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.exception.BusinessException;
+import com.jiawa.train.common.exception.BusinessExceptionEnum;
+import com.jiawa.train.common.resp.PageResp;
+import com.jiawa.train.common.util.SnowUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -131,6 +136,7 @@ public class ConfirmOrderService {
         log.info("查出余票信息：{}", dailyTrainTicket);
 
         // 扣减余票数量，并判断余票是否足够
+        reduceTickets(req, dailyTrainTicket);
 
         // 选座
 
@@ -145,4 +151,48 @@ public class ConfirmOrderService {
         // 为会员增加购票记录
         // 更新确认订单为成功
     }
+
+    /**
+     * 扣减余票数量，并判断余票是否足够
+     * @param req
+     * @param dailyTrainTicket
+     */
+    private static void reduceTickets(ConfirmOrderDoReq req, DailyTrainTicket dailyTrainTicket) {
+        for(ConfirmOrderTicketReq ticketReq: req.getTickets()){
+            String seatTypeCode = ticketReq.getSeatTypeCode();
+            SeatTypeEnum seatTypeEnum = EnumUtil.getBy(SeatTypeEnum::getCode, seatTypeCode);
+            switch(seatTypeEnum){
+                case YDZ ->{
+                    int countLeft = dailyTrainTicket.getYdz() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.BUSINESS_CONFIRM_ORDER_TICKET_NUM_ERROR);
+                    }
+                    dailyTrainTicket.setYdz(countLeft);
+                }
+                case EDZ ->{
+                    int countLeft = dailyTrainTicket.getEdz() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.BUSINESS_CONFIRM_ORDER_TICKET_NUM_ERROR);
+                    }
+                    dailyTrainTicket.setEdz(countLeft);
+                }
+                case RW ->{
+                    int countLeft = dailyTrainTicket.getRw() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.BUSINESS_CONFIRM_ORDER_TICKET_NUM_ERROR);
+                    }
+                    dailyTrainTicket.setRw(countLeft);
+                }
+                case YW ->{
+                    int countLeft = dailyTrainTicket.getYw() - 1;
+                    if (countLeft < 0){
+                        throw new BusinessException(BusinessExceptionEnum.BUSINESS_CONFIRM_ORDER_TICKET_NUM_ERROR);
+                    }
+                    dailyTrainTicket.setYw(countLeft);
+                }
+            }
+        }
+    }
+
+
 }
