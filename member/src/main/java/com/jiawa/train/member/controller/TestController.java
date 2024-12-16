2182d7aea319f51f01e71e9d29dd42bfaaf8025b
JianBing <jianbing0828@163.com>
2024-12-16 17:56:44 +0800

    13.8 集成Nacos注册中心，所有的SpringBoot模块都需连接注册中心，所以在common引入;配置gateway+Nacos，实现按应用名路由转发

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.jiawa.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
+
+    @Autowired
+    Environment environment;
     @GetMapping("/hello")
     public String hello() {
-        return "hello"+ testNacos;
+        String port = environment.getProperty("local.server.port");
+        return "hello"+ " "+testNacos+" "+ "端口："+port;
     }
 }
