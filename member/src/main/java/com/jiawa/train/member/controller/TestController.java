24a5c050392a9943697941a9ab1f6b194716268b
JianBing <jianbing0828@163.com>
2024-12-16 16:00:30 +0800

    13.4 集成Nacos配置中心，实现动态刷新的功能

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.jiawa.train.member.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope // 支持动态刷新; 按规范的好处：以后想换配置中心，不用动java代码
 public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
     @GetMapping("/hello")
     public String hello() {
-        return String.format("hello world! %s", testNacos);
+        return "hello"+ testNacos;
     }
 }
