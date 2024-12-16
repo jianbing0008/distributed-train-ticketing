bba17e142eb2a2ac8b193acb784d7a34ab68afd8
JianBing <jianbing0828@163.com>
2024-12-16 15:12:51 +0800

    13.4 读取配置文件的配置

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.jiawa.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.nacos}")
+    private String testNacos;
     @GetMapping("/hello")
     public String hello() {
-        return "hello world!";
+        return String.format("hello world! %s", testNacos);
     }
 }
